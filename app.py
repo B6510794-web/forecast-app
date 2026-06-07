2ecd8e15e417c8a0a42bd144f4816731815cfa8f
B6510794-web <pitikxrnnn@gmail.com>
2026-06-07 20:33:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ with tab1:
         st.line_chart(chart_data.set_index('เดือน'), height=350)
 
     st.subheader("📋 ตารางตัวเลขพยากรณ์ล่วงหน้า")
-    st.dataframe(forecast_df.T, use_container_width=True)
+   st.table(forecast_df.T)
 
 # 7. แผนรับมือในแท็บที่ 3
 with tab3:
